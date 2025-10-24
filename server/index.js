a5b0339e18a71073b3600d427570ea87fc8c642b
Varad <varadpimple06@gmail.com>
2025-10-24 23:21:56 +0530

    Fix CORS issue to allow all origins

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*'  // allow requests from any domain
+}));
 app.use(express.json());
 
 // Connect to MongoDB using environment variable
